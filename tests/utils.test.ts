b7ed7673d08d1211a85c16b0c909e172fe3179ae
winnie0616 <winniebear1213@gmail.com>
2025-04-09 03:29:23 +0800

    check:lint&prettier

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -4,9 +4,6 @@ test("devide() should return correct result", () => {
   expect(devide(6, 3)).toBe(2);
 });
 
-test.each([1, 2, 100, -5, 0])(
-  "devide(%i, 0) should throw error",
-  (num) => {
-    expect(() => devide(num, 0)).toThrow("Zero addition not allowed");
-  }
-);
+test.each([1, 2, 100, -5, 0])("devide(%i, 0) should throw error", (num) => {
+  expect(() => devide(num, 0)).toThrow("Zero addition not allowed");
+});
